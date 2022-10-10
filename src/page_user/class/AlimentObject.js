c84bca25b64beaa9438e388f9a3a3b9870d2b6d9
rengnareb77~ <luluozoir@gmail.com~>
2022-10-10 11:49:48 +0200

    Ajout de la classe AlimentObject

---
+++ src/page_user/class/AlimentObject.js
@@ -0,0 +1,10 @@
+
+class AlimentObject {
+    constructor(nom,valeurCalorique,isVege,allergenes) {
+        this.nom = nom;
+        this.valeurCalorique = valeurCalorique;
+        this.isVege = isVege;
+        this.allergenes = allergenes;
+    }
+}
+export default AlimentObject;
