94b6f3b940f1b6a25f5a3712e5c55f3c691530bb
rengnareb77~ <luluozoir@gmail.com~>
2022-10-25 17:08:23 +0200

    Ajout des endpoint POST et DELETE de Aliment.

--- api/db.js
+++ api/db.js
@@ -1,3 +1,5 @@
+
+
 const {createPool} = require("mariadb");
 
 const { config } = require('dotenv');
@@ -11,6 +13,7 @@ const DataBase = function (){
         database : process.env.DATABASE
     });
 
+    // Requêtes relatives à restoU
     this.getRestoU = async () =>{
         const conn = await pool.getConnection();
         let restoU = [];
@@ -27,6 +30,23 @@ const DataBase = function (){
             .on("data", data => restoU.push(data))
         await conn.end();
         return restoU;
+    }
+
+    // Requêtes relatives à aliment
+
+    this.createAliment = async (aliment) =>{
+        const conn = await pool.getConnection();
+
+        const request = "INSERT INTO aliment (nom,type,calories,allergene,vegan,nutriscore,description,idChoix,proteines,lipides,portionBase) VALUES (?,?,?,?,?,?,?,?,?,?,?)";
+        await conn.query(request, [aliment.nom, aliment.type, aliment.calories, aliment.allergene, aliment.vegan, aliment.nutriscore, aliment.description, aliment.idChoix, aliment.proteines, aliment.lipides, aliment.portionBase]);
+        await conn.end();
+    }
+
+    this.deleteAliment = async (id) =>{
+        const conn = await pool.getConnection();
+        const request = "DELETE FROM aliment WHERE idAliment = ?";
+        await conn.query(request, [id]);
+        await conn.end();
     }
 }
 
--- api/index.js
+++ api/index.js
@@ -2,13 +2,15 @@ const express = require('express');
 
 const app = express();
 const restoU = require('./routes/restoU');
+const aliment = require('./routes/aliment');
 const port = 3000;
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
-
+app.use(express.json());
 app.use('/api',restoU);
+app.use('/api',aliment);
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
