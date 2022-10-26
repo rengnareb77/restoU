d909e190a1d3729cf3207615e52ce8da2da7c8e1
rengnareb77~ <luluozoir@gmail.com~>
2022-10-26 23:38:19 +0200

    Mise à jour avec gestion de d'erreur

--- api/routes/restoU.js
+++ api/routes/restoU.js
@@ -4,12 +4,15 @@ const db = require('../db');
 const router = express.Router();
 
 router.get("/restoU", async (req, res) => {
-    res.json(await db.getRestoU());
+    db.getRestoU()
+        .then(data => res.set(200).json(data))
+        .catch(()=> res.set(500).send("Erreur lors de la récupération des restaurants"));
 });
 
 router.get("/restoU/:id", async (req, res) => {
     const id = req.params.id;
-    const ru = await db.getRestoUById(id)
-    res.json(ru[0]);
+    db.getRestoUById(id)
+        .then(data => res.set(200).json(data))
+        .catch(()=> res.set(500).send("Erreur lors de la récupération du restaurant"));
 });
 module.exports = router;
