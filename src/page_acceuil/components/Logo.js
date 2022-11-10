928c06d9683742e0c7f0dc3cd831d2fe5ba48924
xflegeau <xavier.flegeau@gmail.com>
2022-11-10 12:14:45 +0100

    frontAcceuil

---
+++ src/page_acceuil/components/Logo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Logo = () => {
+    return (
+        <div className="logo">
+            <img src="./logo192.png" alt="logo de notre application" />
+            <h3>React App RU</h3>
+        </div>
+    );
+};
+
+export default Logo;
